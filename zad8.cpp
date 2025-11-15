8d00e84ddd0e9bded59390c35ffc22140b045a93
bigmastershifu <forrmipt@gmail.com>
2025-11-15 15:01:32 +0300

    Update zad8.cpp

--- zad8.cpp
+++ zad8.cpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <bit>
 
-long long log_2(long long x) {
+long long floor_log_2(long long x) {
   //  глобально тут мы ищем старший бит
   if (x <= 0) {
     return -1;
@@ -17,19 +17,19 @@ long long log_2(long long x) {
   return n;
 }
 
-long long double_log2(double d) {
+long long floor_double_log2(double d) {
   uint64_t bits = std::bit_cast<uint64_t>(d);
   int exp = (bits >> 52) & 0b11111111111; //  сдвинули на 52 позиции получили экспоненту и знак а потом выкинули знак
   return exp - 1023;  //  так надо
 }
 
 int main() {
-  assert(log_2(3) == std::floor(std::log2(3)));
-  assert(log_2(15) == std::floor(std::log2(15)));
-  assert(log_2(100) == std::floor(std::log2(100)));
-  assert(double_log2(3) == std::floor(std::log2(3)));
-  assert(double_log2(1.66) == std::floor(std::log2(1.66)));
-  assert(double_log2(0.3) == std::floor(std::log2(0.3)));
+  assert(floor_log_2(3) == std::floor(std::log2(3)));
+  assert(floor_log_2(15) == std::floor(std::log2(15)));
+  assert(floor_log_2(100) == std::floor(std::log2(100)));
+  assert(floor_double_log2(3) == std::floor(std::log2(3)));
+  assert(floor_double_log2(1.66) == std::floor(std::log2(1.66)));
+  assert(floor_double_log2(0.3) == std::floor(std::log2(0.3)));
 
   return 0;
 }
